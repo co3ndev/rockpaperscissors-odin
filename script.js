c3e3287b2fd67a3176daf292910f553d59ec7922
co3ndev <coenprofessional@icloud.com>
2025-10-02 18:04:46 -0400

    Removed game.html / Add style.css

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+let playerScore = 0;
+let compScore = 0;
+
 function game(playerInput){
     let compChoice = getRandomInt(1, 3);
-    const playerScore = 0;
-    const compScore = 0;
     
     console.log(compChoice);
     if (playerInput === compChoice){
@@ -18,13 +19,13 @@ function game(playerInput){
     if (playerInput === 1 && compChoice === 3 ||
         playerInput === 2 && compChoice == 1 ||
         playerInput === 3 && compChoice === 2){
-            playerScore +1;
-            document.getElementById("player-score").innerText = "Your score: {playerScore}";
+            playerScore = playerScore +1;
+            document.getElementById("player-score").innerText = "Your score: " + playerScore;
             document.getElementById("result").innerText = "Player wins!"
         }
         else {
-            compScore +1;
-            document.getElementById("computer-score").innerText = "Computer score: {compScore}";
+            compScore = compScore +1;
+            document.getElementById("computer-score").innerText = "Computer score: " + compScore;
             document.getElementById("result").innerText = "Computer wins!";
         }
 }
